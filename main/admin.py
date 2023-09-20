0a7c91fb2337bbcc1b8bfcd00e9c03bb27e339a3
Kovr1k <s.mosiencko@gmail.com>
2023-09-20 15:20:56 +0300

    Создание модели Блоков

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Case
+from .models import Case, CaseBlock
 
 # class TestCase(admin.ModelAdmin):
 #     description = ('date_started',),
 #     titleForURL = ('date_started',)
 
 admin.site.register(Case)
+admin.site.register(CaseBlock)
--- main/models.py
+++ main/models.py
@@ -9,3 +9,20 @@ class Case(models.Model):
     def __str__(self):
         return self.title
     
+class CaseBlock(models.Model):
+    fk = models.ForeignKey(Case, on_delete = models.CASCADE)
+    types = [
+        (1, 'Текст'),
+        (2, 'Одно изображение'),
+        (3, 'Слайдер'),
+    ]
+    type = models.IntegerField("Тип", default=1, choices=types)
+    img_1 = models.ImageField("Изображение 1", upload_to='caseBlocks', blank=True)
+    img_2 = models.ImageField("Изображение 2", upload_to='caseBlocks', blank=True)
+    img_3 = models.ImageField("Изображение 3", upload_to='caseBlocks', blank=True)
+    img_4 = models.ImageField("Изображение 4", upload_to='caseBlocks', blank=True)
+    img_5 = models.ImageField("Изображение 5", upload_to='caseBlocks', blank=True)
+    text = models.TextField("Текст", blank=True)
+
+    def __str__(self):
+        return f'{"Блок " + str(self.fk)}'
