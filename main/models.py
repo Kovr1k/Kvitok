484097163a166d91fd8a4405b752363d353e50ee
Kovr1k <s.mosiencko@gmail.com>
2023-09-21 18:08:45 +0300

    Изменение иодели Кейсов

--- main/models.py
+++ main/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Case(models.Model):
     title = models.CharField("Название", max_length=255, default='')
     img = models.ImageField("Обложка", upload_to='case')
-    description = models.TextField("Наполнение", blank=True)
     titleForURL = models.CharField("Преобразование в URL", max_length=255, default='', blank=True)
 
     def __str__(self):
